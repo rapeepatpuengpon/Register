6f94b4757b9d3295d6692f6aed572788256a4d2d
rapeepatpuengpon <rapeepat.pu@ku.th>
2026-09-23 15:33:01 +0700

    Add files via upload

--- Login.java
+++ Login.java
@@ -1,6 +1,7 @@
 import java.awt.*;
 import javax.swing.*;
-public class Login extends JFrame {
+import java.awt.event.*;
+public class Login extends JFrame implements ActionListener{
 
     Container cp;
     JPanel p;
@@ -67,52 +68,9 @@ public class Login extends JFrame {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
-    
-        /*public static void main(String[] args) {
-        Container cp = f.getContentPane();
-        cp.setBackground(Color.LIGHT_GRAY);
-        cp.setLayout(null);
-
-        JPanel p = new JPanel();
-        p.setLayout(null);
-        
-        JLabel l = new JLabel("Registration");
-        l.setBounds(0, 10, 300, 50);
-        l.setHorizontalAlignment(JLabel.CENTER);
-        l.setFont(new Font("", Font.BOLD, 25));
-
-
-        JLabel l1 = new JLabel("USER");
-        JTextField t1 = new JTextField();
-        l1.setBounds(50, 50, 50, 50);
-        t1.setBounds(50,90, 200, 30);        
-        l1.setFont(new Font("", Font.BOLD, 16));
-        
-
-        JLabel l2 = new JLabel("Password");
-        JTextField t2 = new JTextField();
-        l2.setBounds(50, 120, 100, 50);
-        t2.setBounds(50,160, 200, 30);
-        l2.setFont(new Font("", Font.BOLD, 16));
-
-        JButton b = new JButton("Login");
-        b.setBounds(100, 220, 100, 40);
-
-        p.add(l);
-        p.add(l1);
-        p.add(t1);
-        p.add(l2);
-        p.add(t2);
-        p.add(b);
-
-        p.setBounds(95, 80, 300, 300);
-        cp.add(p);
-
-        f.setSize(500, 500);
-        f.setLocationRelativeTo(null);
-        f.setResizable(false);
-        f.setVisible(true);
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
-    }*/
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
+    }
 }
